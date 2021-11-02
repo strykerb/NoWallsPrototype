f27cc6fddb010f350bb1e2b3f1dc90f130ababfe
Finn Morrison <fmorriso@ucsc.edu>
2021-11-01 22:51:55 -0700

    Prefabbed portal

--- Assets/Scripts/PortalCamera.cs
+++ Assets/Scripts/PortalCamera.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class PortalCamera : MonoBehaviour
 {
+    
     public Transform playerCamera;
-    public Transform portal;
-    public Transform targetPortal;
 
-    // Update is called once per frame
+    [Header("This Portal")]
+    public Transform portal;
+
+    [Header("Other Portal")]
+    public Transform targetPortal;
+    public MeshRenderer renderPlane;
+
+    private void Start() {
+        if (!playerCamera) {
+            playerCamera = GameObject.Find("Player").GetComponentInChildren<Camera>().transform;
+        }
+    }
+
     void Update()
     {
+        // get positional difference and set position
         Vector3 playerOffsetFromPortal = playerCamera.position - targetPortal.position;
         transform.position = portal.position + playerOffsetFromPortal;
 
+        // get rotational difference and set rotation
         float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, targetPortal.rotation);
 
         Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
--- Assets/Scripts/PortalTextureSetup.cs
+++ Assets/Scripts/PortalTextureSetup.cs
@@ -10,19 +10,37 @@ public class PortalTextureSetup : MonoBehaviour
     public Material cameraMatA;
     public Material cameraMatB;
 
+    public Shader portalShader;
+    public PortalCamera[] portalCameras;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (cameraA.targetTexture != null) {
-            cameraA.targetTexture.Release();
-        }
-        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
-        cameraMatA.mainTexture = cameraA.targetTexture;
 
-        if (cameraB.targetTexture != null) {
-            cameraB.targetTexture.Release();
+        foreach (PortalCamera portalCamera in portalCameras) {
+            
+            portalCamera.renderPlane.sharedMaterial = new Material(portalShader);
+
+            Camera cam = portalCamera.GetComponent<Camera>();
+
+            if (cam.targetTexture != null){
+                cam.targetTexture.Release();
+            }
+
+            cam.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+            portalCamera.renderPlane.sharedMaterial.mainTexture = cam.targetTexture;
         }
-        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
-        cameraMatB.mainTexture = cameraB.targetTexture;
+
+        // if (cameraA.targetTexture != null) {
+        //     cameraA.targetTexture.Release();
+        // }
+        // cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        // cameraMatA.mainTexture = cameraA.targetTexture;
+
+        // if (cameraB.targetTexture != null) {
+        //     cameraB.targetTexture.Release();
+        // }
+        // cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        // cameraMatB.mainTexture = cameraB.targetTexture;
     }
 }
