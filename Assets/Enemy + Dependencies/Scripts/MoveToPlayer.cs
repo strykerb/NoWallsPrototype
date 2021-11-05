ccba91b0700a0bdc923f0eb3ea7faf70c301dbe6
ro-j0 <63885199+ro-j0@users.noreply.github.com>
2021-11-05 13:59:43 -0700

    Enemy + Basic AI added

---
+++ Assets/Enemy + Dependencies/Scripts/MoveToPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPlayer : MonoBehaviour
+{
+    public Transform goal;              // Player Transform.
+    public float offset = 2f;           // Offset once object is close to player to prevent collision. 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(goal.position, transform.position) >= offset)
+        {
+            Vector3 direction = goal.position - transform.position;
+            transform.Translate(direction * Time.deltaTime);
+        }
+    }
+}
